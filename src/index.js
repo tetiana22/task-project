a4bcaa84cc98bb07569272f8638b21b515c0cc39
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-05-13 18:33:38 +0100

    CH

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { App } from './components/App/App';
 import './index.css';
 import { Provider } from 'react-redux';
