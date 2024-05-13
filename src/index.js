cea6e90516334ec86683ae93666bc73e41047697
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-05-13 18:55:43 +0100

    cg

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/tetiana22/task-project">
+      <BrowserRouter basename="/task-project">
         <PersistGate persistor={persistor} loading={null}>
           <App />
           <ToastContainer
