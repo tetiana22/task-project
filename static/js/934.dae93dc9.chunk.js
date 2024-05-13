01666101b2e549608c61b00a60c0c3a83f5eed6e
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-05-13 17:56:48 +0000

    Deploying to gh-pages from @ tetiana22/task-project@cea6e90516334ec86683ae93666bc73e41047697 🚀

---
+++ static/js/934.dae93dc9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktask_project=self.webpackChunktask_project||[]).push([[934],{2934:function(e,s,c){c(2791);s.Z=c.p+"static/media/icons-sprite.1ec4e8ba868813c362552b31101b3715.svg"}}]);
+//# sourceMappingURL=934.dae93dc9.chunk.js.map
